e7d592c45cb69d9ae1f89d8e96dc17ca60043594
Iván <ivankras@hotmail.com>
2021-06-28 20:56:29 -0500

    Days 6 and 7 (#7)
    * ADD Day 3 First Part Exercises
    
    * Day 3 - Movie Review
    
    * Day 3 - Movie Review
    
    * Day 4 - Responsive w Flexbox
    
    * Day 4 - Responsive w Bootstrap
    
    * Day 4 - Fixes
    
    * Day 4 - Fixes
    
    * Day 5 - JS Intro
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 7 - Functional JS and Mocha
    
    * Day 7 - Functional JS and Mocha
    
    * Day 7 - Functional JS and Mocha
    
    * Day 7 - Regexp
    
    * Day 7 - Regexp
    
    * Day 7 - Functional JS, regexp
    
    * Day 7 - Functional JS, regexp
    
    * Day 7 - Functional JS, regexp

---
+++ decorate/js/utils.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+    'use strict';
+    
+    // Homepage
+    const homepage = document.getElementById('homepage');
+    homepage.onclick = function() { location.href = '/'; };
+
+    // Make text bigger
+    const biggerButton = document.getElementById('bigger');
+    biggerButton.onclick = enlargeText;
+    
+    function enlargeText() {
+        const textarea = document.getElementById('text');
+        textarea.classList.replace('small', 'big');
+    }
+
+    // Bling
+    const blingCB = document.getElementById('bling');
+    blingCB.onchange = toggleDecorate;
+
+    function toggleDecorate() {
+        const textarea = document.getElementById('text');
+        if (blingCB.checked) {
+            textarea.classList.add('decorated');
+            document.body.classList.add('background-dollar');
+        } else {
+            textarea.classList.remove('decorated');
+            document.body.classList.remove('background-dollar');
+        }
+
+    }
+};
