c79cc964ace78f1895842884571401226bda85a8
Iván <ivankras@hotmail.com>
2021-07-06 10:34:06 -0500

    Day 11 (#9)
    * ADD Day 3 First Part Exercises
    
    * Day 3 - Movie Review
    
    * Day 3 - Movie Review
    
    * Day 4 - Responsive w Flexbox
    
    * Day 4 - Responsive w Bootstrap
    
    * Day 4 - Fixes
    
    * Day 4 - Fixes
    
    * Day 5 - JS Intro
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 6 - JS Timers and more
    
    * Day 7 - Functional JS and Mocha
    
    * Day 7 - Functional JS and Mocha
    
    * Day 7 - Functional JS and Mocha
    
    * Day 7 - Regexp
    
    * Day 7 - Regexp
    
    * Day 7 - Functional JS, regexp
    
    * Day 7 - Functional JS, regexp
    
    * Day 7 - Functional JS, regexp
    
    * Da10 - JS inheritance
    
    * Day 10 - JS inheritance
    
    * Day 11 - JS classes
    
    * Day 11 - JS classes
    
    * Day 11 - JS classes

---
+++ classes/js/bank.js
@@ -0,0 +1,43 @@
+class Bank {
+    constructor() {
+        this._accounts = [];
+    }
+
+    static #nextNumber = 0;
+    static getNextNumber() {
+        return Bank.#nextNumber++;
+    }
+
+    addAccount() {
+        const nextNumber = Bank.getNextNumber(); 
+        const newAccount = new Account(nextNumber);
+        this._accounts[nextNumber] = newAccount;
+        return nextNumber;
+    }
+
+    addSavingsAccount(interest) {
+        const nextNumber = Bank.getNextNumber(); 
+        const newAccount = new SavingsAccount(nextNumber, interest);
+        this._accounts[nextNumber] = newAccount;
+        return nextNumber;
+    }
+
+    addCheckingAccount(overdraft) {
+        const nextNumber = Bank.getNextNumber(); 
+        const newAccount = new CheckingAccount(nextNumber, overdraft);
+        this._accounts[nextNumber] = newAccount;
+        return nextNumber;
+    }
+
+    closeAccount(number) {
+        this._accounts[number] = null;
+    }
+
+    accountReport() {
+        return this._accounts.filter(e => e !== null).map(e => e.toString()).join('\n');
+    }
+
+    endOfMonth() {
+        return this._accounts.filter(e => e !== null).map(e => e.endOfMonth()).join('\n');
+    }
+}
